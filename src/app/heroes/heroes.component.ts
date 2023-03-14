6fe1942c47273e4f8034936cac031cd8fe2b4254
KatiaZd <katia.zouad@hotmail.fr>
2023-03-14 13:27:09 +0100

    Ajout du service hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,13 +10,35 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent {
 
-    heroes = HEROES;
+    // Déclaration de la propriété selectedHero
     selectedHero?: Hero;
+
+    // Déclaration de la propriété selectedHero
+    heroes: Hero[] = [];
+    
+    constructor(private heroService: HeroService) {}
+
+    // Observable : méthode appelée au chargement du composant, récupération des héros
+    getHeroes(): void {
+      this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
+    }
+
+    // Méthode appelée au chargement du composant, récupération des héros
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+
+      // Méthode appelée au chargement du composant, récupération des héros - Mauvaise pratique !!
+    // getHeroes(): void {
+    //   this.heroes = this.heroService.getHeroes();
+    // }
+
     
     // Gestion du clic sur un héro
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
-    
+
   }
 
